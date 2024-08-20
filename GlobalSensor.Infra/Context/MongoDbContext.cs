ad24d38f1e390874138a1eaf9ba7109c7eccbca5
Douglas Mariano Silverio <douglas07@gmail.com>
2024-08-20 00:55:10 -0300

    chore: recovered code.
    trying to upload my recovered code.

--- GlobalSensor.Infra/Context/MongoDbContext.cs
+++ GlobalSensor.Infra/Context/MongoDbContext.cs
@@ -1,5 +1,6 @@
 using GlobalSensor.Domain.Entities;
 using GlobalSensor.Infra.Configurations;
+using GlobalSensor.Infra.Models;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 
@@ -13,10 +14,22 @@ public class MongoDbContext
     {
         var client = new MongoClient(configurations.Value.ConnectionString);
         _database = client.GetDatabase(configurations.Value.DatabaseName);
+        
+        CreateCollectionIfNotExists("SensorData");
+
     }
 
-    public IMongoCollection<SensorData> SensorDataCollection => 
-        _database.GetCollection<SensorData>("SensorData");
+    public IMongoCollection<SensorDataDocument> SensorDataCollection => 
+        _database.GetCollection<SensorDataDocument>("SensorData");
+    
+    private void CreateCollectionIfNotExists(string collectionName)
+    {
+        var collections = _database.ListCollectionNames().ToList();
+        if (!collections.Contains(collectionName))
+        {
+            _database.CreateCollection(collectionName);
+        }
+    }
 
     
 }
